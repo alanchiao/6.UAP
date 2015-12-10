4983ccb2b07289935e2260c95d4c10b1fcbb7c9e
Alan Chiao <achiao@lanka.csail.mit.edu>
2015-12-09 20:13:04 -0500

    merge

--- blur/distributed_blur.c
+++ blur/distributed_blur.c
@@ -1,4 +1,11 @@
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <mpi.h>
+#include <time.h>
 
 #include <unistd.h> 
 #include <stdlib.h> 
@@ -13,12 +20,18 @@
 
 #define BLUR_COMM_TAG 5
 
-
 int world_rank;
 int world_size;
-png_bytep * row_pointers;
+
+/**
 png_bytep * row_pointers_post_bh; // bh(x,y)
 png_bytep * row_pointers_post_bv; // bv(x,y)
+**/
+
+png_bytep * row_pointers;    // not used for computations, only in call to read_png_file
+char * image; 							 // each pixel = 3 bytes = 3 chars. 
+char * row_pointers_post_bh; // bh(x,y)
+char * row_pointers_post_bv; // bv(x,y)
 
 void blur(png_info_t *png_info, distributed_info_t *distributed_info) 
 {
@@ -135,17 +148,15 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		abort_("Usage: program_name <file_in> <file_out>");
 
+	// MPI Initialization
 	MPI_Init(&argc, &argv);
-	
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int name_len;
 	MPI_Get_processor_name(processor_name, &name_len);
-
-    printf( "Hello world from processor %s, rank %d of %d\n", processor_name, world_rank, world_size);
+	printf( "Hello world from processor %s, rank %d of %d\n", processor_name, world_rank, world_size);
 
 	// Read file + setup logic
 	png_info_t* png_info = malloc(sizeof(png_info_t));
@@ -153,13 +164,23 @@ int main(int argc, char **argv)
 	row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * png_info->height);
 	for (int y=0; y < png_info->height; y++)
 		row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_info->png_ptr,png_info->info_ptr));
+	/**
 	row_pointers_post_bh = (png_bytep*) malloc(sizeof(png_bytep) * png_info->height);
 	for (int y=0; y < png_info->height; y++)
 		row_pointers_post_bh[y] = (png_byte*) malloc(png_get_rowbytes(png_info->png_ptr,png_info->info_ptr));
 	row_pointers_post_bv = (png_bytep*) malloc(sizeof(png_bytep) * png_info->height);
 	for (int y=0; y < png_info->height; y++)
 		row_pointers_post_bv[y] = (png_byte*) malloc(png_get_rowbytes(png_info->png_ptr,png_info->info_ptr));
+	**/
+
 	read_png_file(&row_pointers, png_info, fp);
+
+	int total_size = png_info->width * png_info->height * sizeof(char) * 3;
+	image = malloc(total_size);
+	row_pointers_post_bh = malloc(total_size);
+	row_pointers_post_bv = malloc(total_size);
+	memset(image, 0, total_size);
+
 	// Distributed preparation logic
 	// Distribute chunks of png
 	int global_height = png_info->height;
@@ -185,16 +206,21 @@ int main(int argc, char **argv)
 	distributed_info->local_max_width = local_max_width;
 	// Define Need and Have regions (N and H)
 	// Blur logic
-	blur(png_info, distributed_info); // region goes from local_min_height to local_max_height - 1
+	
+	struct timespec start;
+	struct timespec end;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	for (int iter = 0; iter < 30; iter++) {
+		blur(png_info, distributed_info); // region goes from local_min_height to local_max_height - 1
+	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	printf("average total time: %f seconds\n", ((end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)/1e9)/30);
+	
 	// Write file + cleanup logic
 	write_png_file(argv[2], &row_pointers_post_bv, png_info);
 
 	free(distributed_info);
-	for (int y = 0; y < png_info->height; y++) {
-		free(row_pointers[y]);
-		free(row_pointers_post_bh[y]);
-		free(row_pointers_post_bv[y]);
-	}
+	free(image)
 	free(row_pointers);
 	free(row_pointers_post_bh);
 	free(row_pointers_post_bv);
